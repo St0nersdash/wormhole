91414bcc77d769f81ead101dc586012a92a2feaf
Josh Siegel <jsiegel@jumptrading.com>
2022-10-21 19:03:29 +0000

    WH-1778: Fix up for where the binaries actually are @ Closes #1778

--- aurora/deploy.sh
+++ aurora/deploy.sh
@@ -7,4 +7,4 @@ mkdir -p ~/.near
 wget -q http://localhost:3031/validator_key.json -O ~/.near/validator_key.json
 node_modules/near-cli/bin/near delete aurora.test.near test.near  # if needed
 node_modules/near-cli/bin/near create-account aurora.test.near --master-account=test.near --initial-balance 1000000
-node_modules/@auroraisnear/cli/lib/aurora.js install --chain 1313161556 --owner test.near bin/mainnet-release.wasm
+node_modules/@auroraisnear/cli/lib/aurora.js install --chain 1313161556 --owner test.near bin/aurora-local.wasm
